5b89c1520b481c30d287bb7376189fa095c42727
baseernm <baseernm@localhost.localdomain>
2025-06-30 09:24:16 +0500

    logout api updated

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,9 +1,10 @@
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 
 export async function GET(request) {
   const cookie = await cookies()
   cookie.delete("token")
-  return NextResponse.redirect(new URL("/", request.url))
+  return redirect("/")
 }
